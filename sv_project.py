bc6211f01473bdfe021c73f826fbf14056cf1604
Kyle Knudson <kyle.knudson2015@gmail.com>
2019-08-05 12:15:43 -0500

    modularized and added optional sections.

--- sv_project.py
+++ sv_project.py
@@ -2,69 +2,67 @@
 Support Vector Machine Project
 Iris Flower Data Set
 '''
-import numpy as np 
-import pandas as pd 
 import matplotlib.pyplot as plt 
-import seaborn as sns 
-
-iris = sns.load_dataset('iris')
-print(iris.info())
-
-'''
-Exploratory Data Analysis
-'''
-
-sns.pairplot(iris, hue = 'species')
-plt.show()
-
-setosa = iris[iris['species']=='setosa']
-sns.jointplot('sepal_length', 'sepal_width', data = setosa, kind = 'kde')
-plt.show()
-
-'''
-Train Test Split
-'''
+import seaborn as sns
 from sklearn.model_selection import train_test_split
-X= iris.drop('species', axis = 1) 
-y = iris['species']
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-
-'''
-Train a model 
-'''
-
 from sklearn.svm import SVC
-model = SVC()
-model.fit(X_train, y_train)
-
-'''
-Model Evaluation
-'''
-predictions = model.predict(X_test)
-
 from sklearn.metrics import confusion_matrix, classification_report
-
-print(confusion_matrix(y_test, predictions))
-print('\n')
-print(classification_report(y_test, predictions))
-
-
-'''
-Grid Search
-'''
 from sklearn.model_selection import GridSearchCV
-param_grid = {'C': [0.1,1,10,100,1000], 'gamma': [0.1,0.01,0.001,0.0001]}
 
-grid = GridSearchCV(SVC(), param_grid, verbose=3)
-grid.fit(X_train, y_train)
+def load_data():
+    iris = sns.load_dataset('iris')
+    print(iris.info())
+    return iris
 
-grid_predictions = grid.predict(X_test)
+def explore_data(dataframe):
+    sns.pairplot(dataframe, hue = 'species')
+    plt.show()
+    setosa = dataframe[dataframe['species']=='setosa']
+    sns.jointplot('sepal_length', 'sepal_width', data = setosa, kind = 'kde')
+    plt.show()
 
-print(classification_report(y_test, grid_predictions))
-print('\n')
-print(confusion_matrix(y_test, grid_predictions))
+def split_data(dataframe):
+    X= dataframe.drop('species', axis = 1)
+    y = dataframe['species']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    return X_train, X_test, y_train, y_test
 
 
+def train_model(X_train, y_train):
+    model = SVC()
+    model.fit(X_train, y_train)
+    return model
+
+def model_evaluation(model, X_test, y_test):
+    predictions = model.predict(X_test)
+    print(confusion_matrix(y_test, predictions))
+    print('\n')
+    print(classification_report(y_test, predictions))
+
+def grid_search(X_train,X_test,y_train, y_test):
+    param_grid = {'C': [0.1,1,10,100,1000], 'gamma': [0.1,0.01,0.001,0.0001]}
+    grid = GridSearchCV(SVC(), param_grid, verbose=3)
+    grid.fit(X_train, y_train)
+    grid_predictions = grid.predict(X_test)
+    print(classification_report(y_test, grid_predictions))
+    print('\n')
+    print(confusion_matrix(y_test, grid_predictions))
+
+
+if __name__ == '__main__':
+    iris = load_data()
+    explore = input('Would you like to explore the data?')
+    if explore == 'yes':
+        explore_data(iris)
+    data = split_data(iris)
+    X_train = data[0]
+    X_test = data[1]
+    y_train = data[2]
+    y_test = data[3]
+    model = train_model(X_train, y_train)
+    model_evaluation(model, X_test, y_test)
+    grid = input('Would you like to run a grid search for the model?')
+    if grid == 'yes':
+        grid_search(X_train, X_test, y_train, y_test)
 
 
